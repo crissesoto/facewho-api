3e937e2250b6bcaf08ae544475f24f13dd79a578
crissesoto <soto.crisse@hotmail.com>
2020-02-25 23:36:42 +0100

    Deploy db to heroku

--- server.js
+++ server.js
@@ -20,10 +20,8 @@ app.use(cors());
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
-      user : 'crissesoto',
-      password : '',
-      database : 'smartBrain'
+      host : process.env.DATABASE_URL,
+      ssl: true
     }
   });
 
